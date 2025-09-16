dff3e4b5a5a56b2301cef0be9c8df06f0dcb4405
younhoso <thdbsgh3443@kakao.com>
2025-09-16 14:47:32 +0900

    Feat: 예시 코드 추가

--- src/provider/ModalProvider.tsx
+++ src/provider/ModalProvider.tsx
@@ -10,3 +10,17 @@ export default function ModalProvider({ children }: { children: ReactNode }) {
   if (!node) return null;
   return reactDom.createPortal(children, node);
 }
+
+/**
+ * 사용예시 코드
+ * export default function Page() {
+ *  return (
+ *     <>
+ *       <h1>페이지 내용</h1>
+ *       <ModalProvider>
+ *         <MyModalContent /> //모달 내부에서 보일 템플릿(컴포넌트)작성
+ *       </ModalProvider>
+ *     </>
+ *   );
+ * }
+ */
